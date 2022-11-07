dcf8be76236ceb5f8220f17d3c1aa991fec5810b
Roger Rangel <roger.rangel.ee@gmail.com>
2022-11-07 18:12:20 +0100

    Adding Tailwind CSS Library, Frontend Setup Complete

--- src/segenie_frontend/src/components/App.jsx
+++ src/segenie_frontend/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "../../assets/styles"
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>APP MAIN PAGE</h1>
+    <div className={` bg-gray-800 min-h-screen`}>
+      <h1 className={`${styles.heading2} text-3xl font-bold underline flex justify-center`}>APP MAIN PAGE</h1>
     </div>
   );
 }
