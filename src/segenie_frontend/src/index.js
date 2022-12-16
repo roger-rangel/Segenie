8d1b729f3f286c53efb64fd3def93669603a1836
Roger Rangel <roger.rangel.ee@gmail.com>
2022-12-16 17:43:09 +0100

    Adding Auth and Internet Identity wallet

--- src/segenie_frontend/src/index.js
+++ src/segenie_frontend/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,17 +14,31 @@ import Web3Authorization from './Views/Web3Authorization/Web3Authorization';
 import '../assets/main.css';
 import '@connect2ic/core/style.css';
 
+// WALLET AND AUTHORimport AuthProvider from './components/AuthProvider/AuthProvider';
+import * as counter from "canisters/counter"
+import { createClient } from "@connect2ic/core"
+import { Connect2ICProvider } from '@connect2ic/react';
+import { InternetIdentity } from '@connect2ic/core/providers';
+import NavProvider from './components/NavProvider/NavProvider';
+import Web3AuthProvider from './components/Web3AuthProvider/Web3AuthProvider';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" exact element={<WelcomePage />} />
-        <Route path="/portal" exact element={<NewPortal />} />
-        <Route path="/options" element={<SegenieOptions />} />
-        <Route path="/web3authorization" element={<Web3Authorization />} />
-      </Route>
-    </Routes>
-  </Router>
+  <Connect2ICProvider providers={[InternetIdentity]}>
+    <Web3AuthProvider>
+      <NavProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="/" exact element={<WelcomePage />} />
+              <Route path="/portal" exact element={<NewPortal />} />
+              <Route path="/options" element={<SegenieOptions />} />
+              <Route path="/web3authorization" element={<Web3Authorization />} />
+            </Route>
+          </Routes>
+        </Router>
+      </NavProvider>
+    </Web3AuthProvider>
+  </Connect2ICProvider>
 );
