3731ae9ae63b77acff7d7d0d5abdbf6f09165656
Roger Rangel <roger.rangel.ee@gmail.com>
2022-12-27 13:55:42 +0100

    Adding the menu for the  Main UI Interface | Part 2

--- src/segenie_frontend/src/Views/Dashboard/components/Menu.jsx
+++ src/segenie_frontend/src/Views/Dashboard/components/Menu.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-// import "./Menu.css";
-// import logo from "../img/logo.png";
-import Icon1 from '../../../../assets/icons/icon-1.svg'
+import Icon1 from '../../../../assets/icons/icon-1.svg';
+import Icon2 from '../../../../assets/icons/icon-2.svg';
+import Icon3 from '../../../../assets/icons/icon-3.svg';
+import Icon4 from '../../../../assets/icons/icon-4.svg';
+import Icon5 from '../../../../assets/icons/icon-5.svg';
 
 const Menu = () => {
   useEffect(() => {
@@ -18,27 +20,23 @@ const Menu = () => {
   }, []);
 
   return (
-    <menu className="w-full h-screen bg-[#19162c] flex flex-col items-center shadow-xl sticky top-0">
+    <menu className="h-screen bg-[#19162c] flex flex-col items-center shadow-xl sticky top-0">
       <img src="./img/logo.png" alt="" className="w-12 pt-8"/>
-      <ul id="mainMenu">
+      <ul id="mainMenu" className="w-full pt-5 flex flex-col items-center">
         <Icon icon={Icon1} />
-        <Icon icon={Icon1} />
-        <Icon icon={Icon1} />
-        <Icon icon={Icon1} />
-        <Icon icon={Icon1} />
+        <Icon icon={Icon2} />
+        <Icon icon={Icon3} />
+        <Icon icon={Icon4} />
+        <Icon icon={Icon5} />
       </ul>
 
-      <ul className="lastMenu">
-        <Icon icon={Icon1} />
-        <Icon icon={Icon1} />
-      </ul>
     </menu>
   );
 }
 
 const Icon = ({ icon }) => (
-  <li>
-    <img src={icon} alt={icon}/>
+  <li className="list-none mx-5 my-0 relative items-center">
+    <img className="pb-5" src={icon} alt={icon}/>
   </li>
 );
 
