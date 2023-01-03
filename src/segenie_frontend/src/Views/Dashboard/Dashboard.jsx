21bedc2fcdd25f0b9c723494be6087734afc85da
Roger Rangel <roger.rangel.ee@gmail.com>
2023-01-03 16:52:06 +0100

    Minting UI | Part 1

--- src/segenie_frontend/src/Views/Dashboard/Dashboard.jsx
+++ src/segenie_frontend/src/Views/Dashboard/Dashboard.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from "./components/Menu";
 import UserMenu from "./components/UserMenu";
+import MintModal from "./components/MintModal";
 import Container from "./components/Container";
 
 const Dashboard = () => {
+  const [modal, setModal] = useState(true);
+  console.log(modal);
   return (
     <div className="w-full h-screen bg-[#121026] flex overflow-hidden scrollbar-hide max-[525px]:overflow-x-hidden max-[525px]:overflow-y-scroll">
-      <Menu />
+      <Menu setModal={setModal} />
+      <MintModal modal={modal} setModal={setModal} />
       <Container />
       <UserMenu />
     </div>
--- src/segenie_frontend/src/Views/Dashboard/components/Menu.jsx
+++ src/segenie_frontend/src/Views/Dashboard/components/Menu.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from "react";
-import Icon1 from '../../../../assets/icons/icon-1.svg';
-import Icon2 from '../../../../assets/icons/icon-2.svg';
-import Icon3 from '../../../../assets/icons/icon-3.svg';
+import React, { useState } from "react";
 import Icon4 from '../../../../assets/icons/icon-4.svg';
-import Icon5 from '../../../../assets/icons/icon-5.svg';
+import menu2 from '../../../../assets/gifs/menu-2.gif';
+import menu3 from '../../../../assets/gifs/menu-3.gif';
+import menu4 from '../../../../assets/gifs/menu-4.gif';
+import Add1 from '../../../../assets/icons/add-1.svg';
+import Add2 from '../../../../assets/gifs/add2.gif';
 
-const Menu = () => {
-  useEffect(() => {
-    const mainMenuLi = document
-      .getElementById("mainMenu")
-      .querySelectorAll("li");
-
-    function changeActive() {
-      mainMenuLi.forEach((n) => n.classList.remove("active"));
-      this.classList.add("active");
-    }
-
-    mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
-  }, []);
+const Menu = ({setModal}) => {
+  const [isShown, setIsShown] = useState(false);
 
   return (
     <menu className="h-screen bg-[#19162c] flex flex-col items-center shadow-xl sticky top-0">
-      <img src="./img/logo.png" alt="" className="w-12 pt-8"/>
+      <img src="./img/logo.png" alt="" className="w-12 pt-4 pb-4"/>
       <ul id="mainMenu" className="w-full pt-5 flex flex-col items-center">
-        <Icon icon={Icon1} />
-        <Icon icon={Icon2} />
-        <Icon icon={Icon3} />
-        <Icon icon={Icon4} />
-        <Icon icon={Icon5} />
+        <li 
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+          onClick={() => setModal(prevModal => !prevModal)}
+          className="list-none mx-5 my-0 relative items-center pb-6">
+          {isShown ? <img className={`rounded-full h-10 w-10 cursor-pointer`} src={Add2} alt={Add2}/> : <img className={`rounded-full h-10 w-10`} src={Add1} alt={Add1}/>}
+        </li>
+
+        <Icon icon={menu3} />
+        <Icon icon={menu2} />
+        <Icon icon={menu4} />
+
+        <li className="list-none mx-5 my-0 relative items-center">
+          <img className="pb-5" src={Icon4} alt={Icon4}/>
+        </li>
       </ul>
     </menu>
   );
 }
 
 const Icon = ({ icon }) => (
-  <li className="list-none mx-5 my-0 relative items-center">
-    <img className="pb-5" src={icon} alt={icon}/>
+  <li className="list-none mx-5 my-0 relative items-center pb-6">
+    <img className="rounded-full h-10 w-10 " src={icon} alt={icon}/>
   </li>
 );
 
